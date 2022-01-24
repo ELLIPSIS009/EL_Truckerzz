486e5f479d9846a4d0d3761ad6744a6577e78926
EL_Abhinav <abhinav2001@outlook.com>
2022-01-24 16:31:21 +0530

    commit

--- commerce/settings.py
+++ commerce/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
