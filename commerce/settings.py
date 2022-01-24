7806f52aca9699f5052f24d77a718f648386f84e
EL_Abhinav <abhinav2001@outlook.com>
2022-01-24 16:30:03 +0530

    commit

--- commerce/settings.py
+++ commerce/settings.py
@@ -125,8 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 
-DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-DISABLE_COLLECTSTATIC=1
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
