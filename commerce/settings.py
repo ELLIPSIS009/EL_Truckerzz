10de8d710cbf92c1c238a672388b01c4505e8594
EL_Abhinav <abhinav2001@outlook.com>
2022-01-24 17:21:35 +0530

    commit

--- commerce/settings.py
+++ commerce/settings.py
@@ -131,7 +131,6 @@ ROOT_PATH = os.path.dirname(__file__)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(ROOT_PATH, 'static'),
     'static',
 ]
 
